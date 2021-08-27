93e0b0e48514e7783d6f3bbe801bfa16c2e6e059
Joey Eamigh <55670930+JoeyEamigh@users.noreply.github.com>
2021-08-26 21:45:22 -0400

    first commit

---
+++ src/components/answers/answers.tsx
@@ -0,0 +1,5 @@
+import 'answers.scss';
+
+export default function Answers() {
+  return <main className="answers"></main>;
+}
---
+++ src/components/first-launch/first-launch.tsx
@@ -0,0 +1,11 @@
+import 'first-launch.scss';
+
+export default function FirstLaunch() {
+  return (
+    <main className="first-launch">
+      <section className="header">
+        <h2>Please configure the extension:</h2>
+      </section>
+    </main>
+  );
+}
